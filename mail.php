24d4e81cd4c640db7301bdce248853209a3921ef
mr-gr1m <alex1111xxx@gmail.com>
2017-06-25 15:34:19 +0300

    go

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+
+$recepient = "alex1111xxx@gmail.com";
+$sitename = "Сайт НАчисто";
+
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $name \nТелефон: $phone";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
